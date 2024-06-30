028bb1459f88ad2cb2b0c35a30b10dfe06aa8651
Luke <lco105@uclive.ac.nz>
2024-06-30 13:08:27 +1200

    Basic working timing system

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "wifi.h"
-
+#include "esp_timer.h"
 #include "esp_system.h"
 
 
@@ -26,6 +26,7 @@
 #define BLUETOOTH_RATE    50
 #define STAT0_LED         18
 #define STAT1_LED         19
+#define IR_PIN            4
 #define CPU_FREQ          160000000 
 #define ADC_READ_LEN      2
 #define STACK_DEPTH       2048
@@ -34,49 +35,49 @@
 #define ADC_CHANNEL         ADC_CHANNEL_4  
 #define ADC_BUFFER_SIZE     1024
 
+static bool ir_beam_cut = 0;
+static esp_timer_handle_t timer_handle;
+
 
 static const char *TAG = "MY_TAG";
 
 typedef struct {
-    uint8_t result;
     bool timer_enable;
     uint64_t start_time;
-    uint64_t finish_time;
-    gptimer_handle_t gptimer;
-    adc_continuous_handle_t handle;
-} adc_params;
+    float lap_time;
+    esp_timer_handle_t gptimer;
+} gptimer_args;
 
+static gptimer_args params;
 
-// static void adc_init()
-// {
-//   adc_continuous_handle_t adc_handle;
+static void install_isr()
+{
+    static bool isr_init = 0;
+    if (!isr_init) {
+        ESP_ERROR_CHECK(gpio_install_isr_service(0));
+        isr_init = true;
+    }
+}
 
-//   adc_continuous_handle_cfg_t adc_config = {
-//     .max_store_buf_size = ADC_BUFFER_SIZE,
-//     .conv_frame_size = ADC_BUFFER_SIZE,
-//   };
+void setPinISR(gpio_num_t pin_num, gpio_int_type_t intr_type, gpio_isr_t interrupt, void* args) 
+{
+    install_isr();
 
-//   //   adc_continuous_new_handle(&adc_config, &adc_handle);
-    
-//     adc_continuous_config_t adc_chan_config = {
-//         .pattern_num = 1,
-//         .sample_freq_hz = 1 * 1000,  // 1 kHz sampling frequency
-
-//     };
-
-//     adc_digi_pattern_config_t digi_cfg = {
-//       .channel = ADC_CHANNEL,
-//     };
-//     adc_continuous_config(adc_handle, &adc_chan_config);
-
-//   // //adc_digi_pattern_config_t adc_pattern = {0};
-//   // adc_chan_config.pattern_num = ADC_CHANNEL;
-//   // //adc_chan_config.adc_pattern = adc_pattern;
-
-// }
+    gpio_set_intr_type(pin_num, intr_type);
+    ESP_ERROR_CHECK(gpio_isr_handler_remove(pin_num));
+    ESP_ERROR_CHECK(gpio_isr_handler_add(pin_num, interrupt, args));
+}
 
 
 
+void ISR_handler(void* args)
+{
+  static bool toggle_led1 = 0;
+  ir_beam_cut = 1;
+  params.lap_time = ((float)(esp_timer_get_time() - params.start_time))/1000000.0;
+  toggle_led1 = !toggle_led1;
+  gpio_set_level(STAT1_LED, toggle_led1);
+}
 
 void task1(void *pvParameters)
 {
@@ -112,27 +113,24 @@ void task2(void *pvParameters)
 
 void task3(void *pvParameters)
 {
-  adc_params *params = (adc_params *)pvParameters;
-
+  gptimer_args *params = (gptimer_args *)pvParameters;
+  bool toggle_led = 0;
+  char msg[21];
   while(1)
   {
     //adc_continuous_read(params->handle, &(params->result), 256, 0, 0);
     //ESP_LOGI(TAG, "Result: %u", params->result);
-    if (params->result > 2000) {
-      if (params->timer_enable) {
-        !(params->timer_enable);
-        gptimer_stop(params->gptimer);
-        params->finish_time = gptimer_get_raw_count(params->gptimer, &(params->finish_time));
-        uint64_t lap_time = params->finish_time - params->start_time;
-        char msg[21];
-        sprintf(msg, "%" PRIu64, lap_time);
-        ESP_LOGI(TAG, "Laptime %u", msg);
-        send_data(0, &msg); // Send laptime over Wifi
-      } else {
-        params->start_time = gptimer_get_raw_count(params->gptimer, &(params->start_time));
-        gptimer_start(params->gptimer);
-        
-      }
+
+
+    if (ir_beam_cut) {
+      toggle_led = !toggle_led;
+      gpio_set_level(STAT0_LED, toggle_led);
+      ir_beam_cut = 0;
+      
+      params->start_time = esp_timer_get_time();
+      sprintf(msg, "%.4f", params->lap_time);
+      ESP_LOGI(TAG, "Laptime %s", msg);
+      send_data(0, msg); // Send laptime over Wifi
     }
     vTaskDelay(pdMS_TO_TICKS(1000));
   }
@@ -140,6 +138,10 @@ void task3(void *pvParameters)
     
 }
 
+
+void timer_callback(void* args) {
+  return;
+}
 
 
 void app_main(void) 
@@ -168,63 +170,27 @@ void app_main(void)
   };
   gpio_config(&stat1_cfg);
 
-
-
-
-  /* Configuring the ADC */
-  uint8_t result = 0;
-  //adc_init();
-
-  adc_continuous_handle_t adc_handle = NULL;
-
-  adc_continuous_handle_cfg_t adc_config = {
-    .max_store_buf_size = ADC_BUFFER_SIZE,
-    .conv_frame_size = ADC_BUFFER_SIZE,
+  const esp_timer_create_args_t timer_args = {
+    .callback = timer_callback,
+    .name = "UCM Timing Gate",
+    .arg = NULL,
+    .dispatch_method = ESP_TIMER_TASK
   };
 
-  //   adc_continuous_new_handle(&adc_config, &adc_handle);
-    
-    adc_continuous_config_t adc_chan_config = {
-        .pattern_num = 1,
-        .sample_freq_hz = 1 * 1000,  // 1 kHz sampling frequency
+  ESP_ERROR_CHECK(esp_timer_create(&timer_args, &timer_handle));
 
-    };
-
-    adc_digi_pattern_config_t digi_cfg = {
-      .channel = ADC_CHANNEL,
-    };
-    adc_continuous_config(adc_handle, &adc_chan_config);
-
-  // //adc_digi_pattern_config_t adc_pattern = {0};
-  // adc_chan_config.pattern_num = ADC_CHANNEL;
-  // //adc_chan_config.adc_pattern = adc_pattern;
-
-  adc_continuous_start(adc_handle);
-
-
-
-
-
-  gptimer_handle_t gptimer = NULL;
-  gptimer_config_t timer_config = {
-      .clk_src = GPTIMER_CLK_SRC_DEFAULT,
-      .direction = GPTIMER_COUNT_UP,
-      .resolution_hz = 1 * 1000 * 1000, // 1MHz, 1 tick = 1us
-  };
-  gptimer_new_timer(&timer_config, &gptimer);
-  gptimer_enable(gptimer);
-
-
-  adc_params params;
   params.timer_enable = 0;
-  params.result = result;
-  params.handle = adc_handle;
-  params.gptimer = gptimer;
+  params.gptimer = timer_handle;
+
+  setPinISR(IR_PIN, GPIO_INTR_NEGEDGE, ISR_handler, NULL); 
 
 
+  params.start_time = esp_timer_get_time();
+  ESP_ERROR_CHECK(esp_timer_start_periodic(params.gptimer, 1000));
 
-  xTaskCreate(task1,"Blink LED 1", STACK_DEPTH, NULL, 1, NULL);
-  xTaskCreate(task2,"Blink LED 2", STACK_DEPTH, NULL, 1, NULL);
+
+  //xTaskCreate(task1,"Blink LED 1", STACK_DEPTH, NULL, 1, NULL);
+  //xTaskCreate(task2,"Blink LED 2", STACK_DEPTH, NULL, 1, NULL);
   xTaskCreate(task3,"Read ADC", STACK_DEPTH, &params, 1, NULL);
 
   
